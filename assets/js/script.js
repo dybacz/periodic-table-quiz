0b5355a262129231228fa82924e69a4e2a9ca0e3
dybacz <jdybacz@gmail.com>
2021-08-20 16:57:19 +0000

    +text-symbol attr to h1. +runQuiz basic function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,37 @@
+var elements = document.getElementsByClassName("element-tile");
+console.log(elements);
+
 document.addEventListener("DOMContentLoaded",function() {
-    let elements = document.getElementsByClassName("element-tile");
-    console.log(elements);
 
     for (let element of elements) {
         element.addEventListener("click", function () {
             let elementName = this.getAttribute("element-name");
             let elementSymbol = this.getAttribute("element-symbol");
+            let elementNumber = parseInt(this.getAttribute("element-number"));
             console.log(elementSymbol);
-            if (this.getAttribute("element-number") === "1") {
+            console.log(elementNumber);
+            if  (elementNumber < 119) {
                 alert(`You Clicked: ${elementSymbol} - ${elementName}`);
+                console.log();
             } else {
-                alert(`You Clicked: ${elementSymbol} - ${elementName}`);
+                alert(`Error! This element is not in the periodic table.`);
             }
         })
     }
+
+    runQuiz();
 })
+
+
+
+function runQuiz () {
+
+    let elementStyles = document.getElementsByClassName("text-symbol")
+    console.log(elementStyles);
+
+    for (let elementStyle of elementStyles) {
+        elementStyle.innerHTML="?";
+        elementStyle.style.backgroundColor = "grey";
+    }
+
+}
