16df847e7ea70c70961527bbdace7d4fba677c59
dybacz <jdybacz@gmail.com>
2021-08-22 12:09:53 +0000

    +Elementname below new element card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var elements = document.getElementsByClassName("element-tile");
 console.log(elements);
 
+var styleSheet = document.styleSheets[0];
+
 document.addEventListener("DOMContentLoaded",function() {
 
     for (let element of elements) {
@@ -42,5 +44,9 @@ function createElement() {
     let createElement = document.createElement('div');
     createElement.innerHTML = '<h1 class="text-symbol new-element">H</h1>';
     createElement.classList.add('element-tile', 'new-ele-bdr'); 
+    createElement.setAttribute('element-name', 'Hydrogen');
     document.getElementById("question-area").appendChild(createElement); 
+    styleSheet.insertRule ('.new-ele-bdr::after {content: "" attr(element-name) "";font-size: 0.6em;font-weight: bold;color: #000000;position: relative;bottom: 0px;left: -3px;}', 41);
+    console.log (styleSheet);
+
 }
