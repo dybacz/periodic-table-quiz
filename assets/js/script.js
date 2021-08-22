198f08f4585b5d55096c150aab3a621951b89d20
dybacz <jdybacz@gmail.com>
2021-08-22 14:51:28 +0000

    +add to array function, + Shuffle array function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
 var elements = document.getElementsByClassName("element-tile");
 console.log(elements);
 
+var elementTable = new Array();
+
 var styleSheet = document.styleSheets[0];
+
 
 document.addEventListener("DOMContentLoaded",function() {
 
+    
+
     for (let element of elements) {
+        
+        let eleName = element.getAttribute("element-name");
+        let eleNum = element.getAttribute("element-number");
+        let eleSym = element.getAttribute("element-symbol");
+        addToArray(eleName, eleNum, eleSym);
+
         element.addEventListener("click", function () {
-            let elementName = this.getAttribute("element-name");
-            let elementSymbol = this.getAttribute("element-symbol");
-            let elementNumber = parseInt(this.getAttribute("element-number"));
-            console.log(elementSymbol);
-            console.log(elementNumber);
-            if  (elementNumber < 119) {
-                alert(`You Clicked: ${elementSymbol} - ${elementName}`);
+
+            console.log(eleSym);
+            console.log(eleNum);
+            if  (eleNum < 119) {
+                alert(`You Clicked: ${eleSym} - ${eleName}`);
                 console.log();
             } else {
                 alert(`Error! This element is not in the periodic table.`);
@@ -22,7 +31,15 @@ document.addEventListener("DOMContentLoaded",function() {
     }
 
     runQuiz();
+    
 })
+
+function addToArray (name, num, symbol) {
+    elementTable.push([name, num, symbol]);
+
+}
+
+
 
 
 function runQuiz () {
@@ -35,6 +52,11 @@ function runQuiz () {
         elementStyle.style.backgroundColor = "grey";
     }
 
+    shuffleArray();
+
+    console.log(elementTable);
+    
+
     createElement();
 
 }
@@ -43,10 +65,19 @@ function createElement() {
     
     let createElement = document.createElement('div');
     createElement.innerHTML = '<h1 class="text-symbol new-element">H</h1>';
-    createElement.classList.add('element-tile', 'new-ele-bdr'); 
+    createElement.classList.add('new-element', 'new-ele-bdr'); 
     createElement.setAttribute('element-name', 'Hydrogen');
     document.getElementById("question-area").appendChild(createElement); 
-    styleSheet.insertRule ('.new-ele-bdr::after {content: "" attr(element-name) "";font-size: 0.6em;font-weight: bold;color: #000000;position: relative;bottom: 0px;left: -3px;}', 41);
+    styleSheet.insertRule ('.new-ele-bdr::after {content: "" attr(element-name) "";font-size: 0.6em;font-weight: bold;color: #000000;position: relative;bottom: 10px;left: -3px;}', 41);
     console.log (styleSheet);
 
+}
+
+function shuffleArray() {
+    for (let i = elementTable.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let k = elementTable[i];
+        elementTable[i] = elementTable[j];
+        elementTable[j] = k;
+      }
 }
